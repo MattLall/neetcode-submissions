13adc27f28f0cc01d04ee717e5fa036ac305e775
Matt Lalliard <97709461+MattLall@users.noreply.github.com>
2026-09-22 15:43:50 +1000

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()){
+            return false;
+        }
+        int l=0,r=s1.length()-1;
+        HashMap<String,Integer> map = new HashMap<>();
+        String s1Key = freq(s1);
+
+        for(;r<s2.length();l++,r++){
+            String s = s2.substring(l,r+1);
+            String key = freq(s);
+            // map.put(key,map.getOrDefault(key,0)+1);
+            if(key.equals(s1Key)) return true;
+        }
+        // while(r<s2.length()){
+        //     map.put(s2.charAt(r),map.getOrDefault(s2.charAt(r),0)+1);
+        //     if(r-l<length){
+        //         r++;
+        //         continue;
+        //     }else{
+        //         map.put(s2.charAt(l),map.get(s2.charAt(l))-1);
+        //         l++;
+        //         r++;
+        //     }
+        // }
+
+        return false;
+    }
+
+    public String freq(String s){
+        int[] freq = new int[26];
+        for(int i = 0; i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+
+        return Arrays.toString(freq);
+    }
+}
